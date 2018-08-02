90a1a238f74430c08e5492ebbbed4c5035786918
Diego Torres <diegotorres50@hotmail.com>
2018-08-02 16:05:32 -0500

    ajustando errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { NavController } from 'ionic-angular';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
-
+  navCtrl:NavController = null; // please add this line as default generated page plus the --ts option on command line
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, navCtrl: NavController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.navCtrl = navCtrl;
     });
   }
 
@@ -25,10 +27,7 @@ export class MyApp {
     // push another page onto the navigation stack
     // causing the nav controller to transition to the new page
     // optional data can also be passed to the pushed page.
-    this.navCtrl.push(page, {
-      id: "123",
-      name: "Diego"
-    });
+    this.navCtrl.setRoot(HomePage);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,8 +11,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { NavController } from 'ionic-angular';
 
 @NgModule({
   declarations: [
